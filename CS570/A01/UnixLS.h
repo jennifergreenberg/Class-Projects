2e85286a25f674c36231c34403ba153e73c3c470
Jessica To <j.to93@yahoo.com>
2015-02-16 17:56:19 -0800

    Create UnixLS.h

---
+++ CS570/A01/UnixLS.h
@@ -0,0 +1,42 @@
+/* Bryant To, Jessica To
+** CS570 - Operating Systems A01*/
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <dirent.h>
+#include <cstdlib>
+using namespace std;
+
+class UnixLS{
+public:
+	// Constructor
+	UnixLS(int argc, const char **args);
+
+	// Opens and reads the appropriate directories
+	void handleDir();
+
+private:
+	DIR *directory;
+
+	// Stores number of arguments in input
+	int size;
+
+	// Flags for hidden files and whether directories have been specified
+	bool readHidden;
+	bool specDir;
+
+	// Vector data structure holding delimited input arguments
+	vector<string> arg;
+
+	// Check input arguments and sets boolean flags
+	void checkArguments();
+
+	// Prints the files found in specified directories
+	void printFiles();
+
+	// Directory error handling
+	void checkDir(int i);
+};
